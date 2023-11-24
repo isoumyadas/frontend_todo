24371f305cf506d62ecc69eb3229f1d7400c94fb
TheSoumyaDas <das8soumya@gmail.com>
2023-11-25 00:27:30 +0530

    Updated the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./styles/app.scss";
 import { createContext } from "react";
 
-export const server = "https://nodejs-todoapp-ykqi.onrender.com/api/v1/";
+export const server = "https://nodejs-todoapp-ykqi.onrender.com/api/v1";
 
 export const Context = createContext({ isAuthenticated: false });
 
